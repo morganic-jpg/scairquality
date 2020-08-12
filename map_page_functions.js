aba4c96f1e86ca11a6e0e5b1b0a206b8a2b230f5
martinwpl <martinwplegal@gmail.com>
2020-08-12 14:00:21 -0700

    Working

--- map_page_functions.js
+++ map_page_functions.js
@@ -542,7 +542,7 @@
             function drawChart(sensor, element, data)
             {
                     console.log('Placing chart at: ' + element);
-                   /* var mychart = Highcharts.chart(element, {
+                    var mychart = Highcharts.chart(element, {
                         chart: {
                             zoomType: 'x'
                         },
@@ -552,8 +552,7 @@
                                 }
                         },
                         xAxis: {
-                                type: 'datetime',
-                                tickWidth: 1
+                                type: 'datetime'
                         },
                         title: {
                                 text: 'Air Quality For Sensor: ' + sensor
@@ -567,60 +566,5 @@
                             name: 'A Channel',
                             data: data
                         }]
-                        });*/
-            var mychart = Highcharts.chart(element, {
-            chart: {
-                zoomType: 'x'
-            },
-            title: {
-                text: 'USD to EUR exchange rate over time'
-            },
-            subtitle: {
-                text: document.ontouchstart === undefined ?
-                    'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-            },
-            xAxis: {
-                type: 'datetime'
-            },
-            yAxis: {
-                title: {
-                    text: 'Exchange rate'
-                }
-            },
-            legend: {
-                enabled: false
-            },
-            plotOptions: {
-                area: {
-                    fillColor: {
-                        linearGradient: {
-                            x1: 0,
-                            y1: 0,
-                            x2: 0,
-                            y2: 1
-                        },
-                        stops: [
-                            [0, Highcharts.getOptions().colors[0]],
-                            [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                        ]
-                    },
-                    marker: {
-                        radius: 2
-                    },
-                    lineWidth: 1,
-                    states: {
-                        hover: {
-                            lineWidth: 1
-                        }
-                    },
-                    threshold: null
-                }
-            },
-
-            series: [{
-                type: 'area',
-                name: 'USD to EUR',
-                data: data
-            }]
-        });
+                        });
     }
